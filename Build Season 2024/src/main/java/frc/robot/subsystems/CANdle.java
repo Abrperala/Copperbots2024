0716bc0ab76edee987d2917242e5275f1864bd3c
Abrperala <24peralaa@students.clkschools.org>
2024-01-23 16:23:35 -0500

    added candle, need to wait on update ctre

---
+++ Build Season 2024/src/main/java/frc/robot/subsystems/CANdle.java
@@ -0,0 +1,59 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class CANdle extends SubsystemBase {
+    // private final CANdle m_candle = new CANdle(Constants.CANdleID, "rio");
+
+    // public LEDState ledstate;
+
+    public CANdle() {
+
+        // CANdleConfiguration configAll = new CANdleConfiguration();
+        // configAll.statusLedOffWhenActive = true;
+        // configAll.disableWhenLOS = false;
+        // configAll.stripType = LEDStripType.RGB;
+        // configAll.brightnessScalar = 0.1;
+        // m_candle.configAllSettings(configAll, 100);
+        // ledstate = LEDState.RED;
+
+        // setLEDSTate(ledstate);
+    }
+
+    // public void setLEDSTate(LEDState state) {
+
+    // ledstate = state;
+    // m_candle.setLEDs(state.r, state.g, state.b);
+
+    // }
+
+    // public LEDState getLEDState() {
+
+    // return ledstate;
+
+    // }
+
+    // public enum LEDState {
+
+    // CUBE(Constants.PURPLE_R, Constants.PURPLE_G, Constants.PURPLE_B),
+    // CONE(Constants.YELLOW_R, Constants.YELLOW_G, Constants.YELLOW_B),
+    // RED(Constants.RED_R, Constants.RED_G, Constants.RED_B);
+
+    // public final int r;
+    // public final int g;
+    // public final int b;
+
+    // private LEDState(int r, int g, int b) {
+    // this.r = r;
+    // this.g = g;
+    // this.b = b;
+    // }
+
+    // }
+
+}
